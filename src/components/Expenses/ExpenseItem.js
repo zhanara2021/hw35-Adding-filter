558e8167f81f0d2406cfbe29da4a1f6bacb0409c
zhanara2021 <89254369+zhanara2021@users.noreply.github.com>
2022-03-10 14:09:41 +0600

    src folders uploaded

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,23 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../../UI/Card';
+import React from 'react';
+
+
+function ExpenseItem(props) {
+    const expenseDate = props.date;
+    const expenseAmount = props.amount;
+    const expenseTitle= props.title; 
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date = {expenseDate}/>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
+            </div>
+        </Card>
+    );
+}
+
+export default ExpenseItem;
